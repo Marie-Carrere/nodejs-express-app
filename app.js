dbd4e422bfac1ca60ca39d1ed7096250d2285a64
Marie-Carrere <marie.carrere@icloud.com>
2018-10-13 16:45:26 +0200

    rendering the view

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var todoController = require('./Controller/TodoController');
+var todoController = require('./controllers/todoController');
 
 var app = express();
 
@@ -9,9 +9,10 @@ app.set('view engine', 'ejs');
 //static files
 app.use(express.static('./public'));
 
+//fire controller
+todoController(app);
+
 //listen to port
 app.listen(3000);
 console.log('Listening to port 3000');
 
-//fire controller
-todoController(app);
