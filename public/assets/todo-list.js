dfcda551a32b581d3be65f2d59a54cae6f5378c3
Marie-Carrere <marie.carrere@icloud.com>
2018-10-13 18:35:30 +0200

    POST request done

--- public/assets/todo-list.js
+++ public/assets/todo-list.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $('form').on('submit', function(){
+  $('form').on('submit', function() {
 
       var item = $('form input');
       var todo = {item: item.val()};
@@ -19,7 +19,7 @@ $(document).ready(function(){
 
   });
 
-  $('li').on('click', function(){
+  $('li').on('click', function() {
       var item = $(this).text().replace(/ /g, "-");
       $.ajax({
         type: 'DELETE',
